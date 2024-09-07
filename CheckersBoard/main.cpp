c83ab3173309f95fe5843e5b37424a7c86e2cf90
devnom7 <nomx.0492@gmail.com>
2024-09-07 12:54:52 -0700

    Initial commit for CheckersBoard project

--- CheckersBoard/main.cpp
+++ CheckersBoard/main.cpp
@@ -7,8 +7,33 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Simple checkerboard that utilizes:
+// for loops
+// modulus operator
+// and nested loops
+
+// Main function
+int main(){
+    // Variables to store checkerboard colors
+    char black = 'B';
+    char red = 'R';
+    
+    // Outer loop controls rows
+    for (int i = 0; i < 8; i++) {
+        // Inner loop controls columns
+        for (int j = 0; j < 8; j++) {
+            // Alternating between black and red using modulus operator based on row and column sum
+            if ((i + j) % 2 == 0) {
+                std::cout << black << " ";  // Print black with a space
+            } else {
+                std::cout << red << " ";    // Print red with a space
+            } // End of if-else statement
+        } // End of inner loop (columns)
+        std::cout << "\n"; // New line after finishing one row in the loop
+    } // End of outer loop (rows)
+    
+    // space under last row
+    std::cout<<"\n";
+    
     return 0;
 }
